8b6de52b7f01fbc1d80d71b0a5bfaa347a2659b0
William <31012583+williamchildres@users.noreply.github.com>
2026-03-14 20:07:11 -0500

    Restore stock packaged WinUI startup shape

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,32 +2,20 @@ using Microsoft.UI.Xaml;
 
 namespace PeopleCodeIDECompanion;
 
-/// <summary>
-/// Provides application-specific behavior to supplement the default Application class.
-/// </summary>
 public partial class App : Application
 {
     public static Window? MainWindow { get; private set; }
+    private Window? _window;
 
     public App()
     {
-        StartupDiagnostics.WriteBreadcrumb("app-ctor-entered");
         InitializeComponent();
-        StartupDiagnostics.WriteBreadcrumb("app-initializecomponent-complete");
-        UnhandledException += OnUnhandledException;
     }
 
     protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
     {
-        StartupDiagnostics.WriteBreadcrumb("onlaunched-entered");
         MainWindow = new MainWindow();
-        StartupDiagnostics.WriteBreadcrumb("mainwindow-constructed");
-        MainWindow.Activate();
-        StartupDiagnostics.WriteBreadcrumb("mainwindow-activated");
-    }
-
-    private static void OnUnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
-    {
-        StartupDiagnostics.Log(e.Exception, "App.UnhandledException");
+        _window = MainWindow;
+        _window.Activate();
     }
 }
